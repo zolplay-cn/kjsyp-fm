7d6a645b3085f9e8b9876f539e573fa28944ab29
Cali Castle <hi@cali.so>
2023-07-19 01:07:46 +0800

    Update podcast.config.ts

--- podcast.config.ts
+++ podcast.config.ts
@@ -11,11 +11,24 @@ export const podcastConfig: PodcastConfig = {
    *   Apple Podcasts, Google Podcasts, Spotify, Stitcher, Overcast,
    *   Pocket Casts Castro, 小宇宙, 哔哩哔哩, YouTube
    */
-  directories: [],
+  directories: [
+    'https://open.spotify.com/show/4vpLJCVh64qh4MOgvHwPeE',
+    'https://podcasts.apple.com/us/podcast/%E7%A7%91%E6%8A%80%E5%8F%8C%E7%9C%BC%E5%95%A4/id1642252327',
+    'https://podcasts.google.com/feed/aHR0cHM6Ly9ranN5cC5mbS9mZWVk',
+  ],
   /**
    * Step 2. Add your podcast hosts here
    */
-  hosts: [],
+  hosts: [
+    {
+      name: 'Cali Castle',
+      link: 'https://twitter.com/thecalicastle',
+    },
+    {
+      name: 'Rather Jie',
+      link: 'https://twitter.com/RatherJie',
+    },
+  ],
 }
 
 export const getPodcast = cache(async () => {
